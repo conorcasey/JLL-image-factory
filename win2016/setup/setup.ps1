9339ecf380ecc4aead105eadac52e834d1302c90
conor-casey <conor.casey@thinkahead.com>
2019-10-23 10:51:52 -0500

    updates from testing

--- win2016/setup/setup.ps1
+++ win2016/setup/setup.ps1
@@ -2,11 +2,8 @@ $ErrorActionPreference = "Stop"
 
 # Switch network connection to private mode
 # Required for WinRM firewall rules
-#$profile = Get-NetConnectionProfile
-#Set-NetConnectionProfile -Name $profile.Name -NetworkCategory Private
-
-# Create a file for testing
-New-Item -Path "c:\" -Name "testfile1" -ItemType "file" -Value "this is test file 1"
+$profile = Get-NetConnectionProfile
+Set-NetConnectionProfile -Name $profile.Name -NetworkCategory Private
 
 # Enable WinRM service
 winrm quickconfig -quiet
@@ -16,6 +13,4 @@ winrm set winrm/config/service/auth '@{Basic="true"}'
 # Reset auto logon count
 # https://docs.microsoft.com/en-us/windows-hardware/customize/desktop/unattend/microsoft-windows-shell-setup-autologon-logoncount#logoncount-known-issue
 Set-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon' -Name AutoLogonCount -Value 0
-
-New-Item -Path "c:\" -Name "testfile2" -ItemType "file" -Value "this is test file 2"
 
