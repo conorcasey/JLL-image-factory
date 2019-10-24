b4a3c4ea5a2d889dfc758c2fbfa5966bb22ec667
conor-casey <conor.casey@thinkahead.com>
2019-10-24 13:39:58 -0500

    adding powershell script for windows updates

--- win2016/setup/setup.ps1
+++ win2016/setup/setup.ps1
@@ -1,5 +1,11 @@
 $ErrorActionPreference = "Stop"
 
+#Get username and password from autounattend
+[xml]$xml = get-content "a:\Autounattend.xml"
+$component = $xml.unattend.settings|Where-Object{$_.pass -eq "oobeSystem"}
+$localadminpw = $component.component.UserAccounts.LocalAccounts.LocalAccount.Password.Value
+$localadminuser = $component.component.UserAccounts.LocalAccounts.LocalAccount.name
+
 # Switch network connection to private mode
 # Required for WinRM firewall rules
 $profile = Get-NetConnectionProfile
@@ -10,7 +16,24 @@ winrm quickconfig -quiet
 winrm set winrm/config/service '@{AllowUnencrypted="true"}'
 winrm set winrm/config/service/auth '@{Basic="true"}'
 
-# Reset auto logon count
-# https://docs.microsoft.com/en-us/windows-hardware/customize/desktop/unattend/microsoft-windows-shell-setup-autologon-logoncount#logoncount-known-issue
-Set-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon' -Name AutoLogonCount -Value 0
+Start-Process ("$filepath\$filename") $UnattendedArgs -Wait -Passthru
+
+Install-PackageProvider -Name NuGet -Force
+
+Find-module -Name PSWindowsUpdate
+Install-Module -Name PSWindowsUpdate -Force
+Find-Module -Name Autologon
+Install-Module -Name Autologon -Force
+
+Copy-Item -path "a:\UpdateTask.ps1" -Destination "C:\Windows\temp\UpdateTask.ps1" -Force
+
+$action = New-ScheduledTaskAction -Execute 'Powershell.exe' -Argument '-ExecutionPolicy Bypass -file "C:\Windows\temp\UpdateTask.ps1" -noexit'
+$trigger =  New-ScheduledTaskTrigger -AtLogOn
+Register-ScheduledTask -Action $action -Trigger $trigger -TaskName "PSWindowsUpdate"
+
+Import-Module -Name Autologon -force;
+Enable-AutoLogon -Username $localadminuser -Password (ConvertTo-SecureString -String $localadminpw -AsPlainText -Force) -LogonCount "1"
+
+Restart-Computer -Force
+
 
