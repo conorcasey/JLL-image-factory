76e5b5d4022c793653fce3021307dc0bac6a980d
conor-casey <conor.casey@thinkahead.com>
2019-10-24 17:09:20 -0500

    testing powershell script for windows updates

--- win2016/setup/setup.ps1
+++ win2016/setup/setup.ps1
@@ -37,4 +37,4 @@ Restart-Computer -Force
 
 # Reset auto logon count
 # https://docs.microsoft.com/en-us/windows-hardware/customize/desktop/unattend/microsoft-windows-shell-setup-autologon-logoncount#logoncount-known-issue
-Set-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon' -Name AutoLogonCount -Value 0
+#Set-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon' -Name AutoLogonCount -Value 0
